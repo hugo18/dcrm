2af470382820c520e01823105ae149f2f7911bbe
Hugo Alves Silva <hugoalves@egresso.ufg.br>
2025-02-04 18:44:09 -0300

    Projeto completo

--- website/forms.py
+++ website/forms.py
@@ -1,11 +1,12 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django import forms
+from .models import Record
 
 class SignUpForm(UserCreationForm):
     email = forms.EmailField(label='', widget=forms.TextInput(attrs={'class':'form-control', 'placeholder': 'Endereço de email'}))
-    first_name = forms.CharField(label='',max_length=100, widget=forms.TextInput(attrs={'class':'form-control', 'placeholder': 'Nome'}))
-    last_name =  forms.CharField(label='',max_length=100, widget=forms.TextInput(attrs={'class':'form-control', 'placeholder': 'Sobrenome'}))
+    first_name = forms.CharField(label='',max_length=50, widget=forms.TextInput(attrs={'class':'form-control', 'placeholder': 'Nome'}))
+    last_name =  forms.CharField(label='',max_length=50, widget=forms.TextInput(attrs={'class':'form-control', 'placeholder': 'Sobrenome'}))
 
     class Meta:
         model = User
@@ -28,3 +29,16 @@ class SignUpForm(UserCreationForm):
         self.fields['password2'].widget.attrs['placeholder'] = 'Confirmar senha'
         self.fields['password2'].label = ''
         self.fields['password2'].help_text =  '<span class="form-text text-muted"><small>Digite novamente sua senha, para verificação.</small></span>'
+
+
+class AddRecordForm(forms.ModelForm):
+    first_name = forms.CharField(required=True, label='', widget=forms.widgets.TextInput(attrs={'class':'form-control', 'placeholder': 'Nome'}))
+    last_name =  forms.CharField(required=True, label='', widget=forms.widgets.TextInput(attrs={'class':'form-control', 'placeholder': 'Sobrenome'}))
+    email = forms.EmailField(required=True, label='', widget=forms.widgets.TextInput(attrs={'class':'form-control', 'placeholder': 'Endereço de email'}))
+    address = forms.CharField(required=True, label='', widget=forms.widgets.TextInput(attrs={'class':'form-control', 'placeholder': 'Endereço'}))
+    city = forms.CharField(required=True, label='', widget=forms.widgets.TextInput(attrs={'class':'form-control', 'placeholder': 'Cidade'}))
+    state = forms.CharField(required=True, label='', widget=forms.widgets.TextInput(attrs={'class':'form-control', 'placeholder': 'Estado'}))
+    
+    class Meta:
+        model = Record
+        exclude = ("user",)
--- website/views.py
+++ website/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
-from .forms import SignUpForm
+from .forms import SignUpForm, AddRecordForm
+from .models import Record
 
 # Create your views here.
 def home(request):
+    records = Record.objects.all()  
     if request.method == 'POST':
         username = request.POST['username']
         password = request.POST['password']
@@ -18,7 +20,7 @@ def home(request):
             messages.success(request, "Ocorreu algum erro no Login. Tente novamente.")
             return redirect('home')
     else:
-        return render(request, 'home.html', {})
+        return render(request, 'home.html', {'records':records})
 
 
 
@@ -42,3 +44,49 @@ def register_user(request):
         form = SignUpForm()
         
     return render(request, 'register.html', {'form': form})
+
+
+def custumer_record(request, pk):
+    if request.user.is_authenticated:
+        custumer_record = Record.objects.get(id=pk)
+        return render(request, 'record.html', {'custumer_record':custumer_record})
+    else:    
+        messages.success(request, 'Você precisa estar logado para ver esta página.')
+        return redirect('home')
+
+def delete_record(request, pk):
+    if request.user.is_authenticated:
+        delete_it = Record.objects.get(id=pk) 
+        delete_it.delete() 
+        messages.success(request, 'Registro apagado!')
+        return redirect('home')
+    else:    
+        messages.success(request, 'Você precisa estar logado para apagar este cadastro.')
+        return redirect('home')
+
+def add_record(request):
+    form = AddRecordForm(request.POST or None)
+    if request.user.is_authenticated:
+        if request.method == 'POST':
+            if form.is_valid():
+                add_record = form.save()
+                messages.success(request, 'Cadastrado!')
+                return redirect('home')
+                
+        return render(request, 'add_record.html', {'form':form})
+    else:    
+        messages.success(request, 'Você precisa estar logado para cadastrar.')
+        return redirect('home')
+
+def update_record(request, pk):
+	if request.user.is_authenticated:
+		current_record = Record.objects.get(id=pk)
+		form = AddRecordForm(request.POST or None, instance=current_record)
+		if form.is_valid():
+			form.save()
+			messages.success(request, "Cadastro atualizado!")
+			return redirect('home')
+		return render(request, 'update_record.html', {'form':form})
+	else:
+		messages.success(request, "Você precisa estar logado para atualizar um cadastro.")
+		return redirect('home')
